89b41e2338196fa7d9c8c77c24a62a2943fe5ad8
Andrii <111363234+malandrii@users.noreply.github.com>
2024-11-09 23:07:05 -0500

    Week 11

---
+++ Week 11/Publication.h
@@ -0,0 +1,30 @@
+// Andrii Malakhovtsev
+// CIS 1202 501
+// November 9, 2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum PublicationType { BOOK, MAGAZINE, NEWSPAPER, AUDIO, VIDEO };
+
+class Publication
+{
+private:
+	string title;
+	string publisher;
+	double price;
+	int year;
+	PublicationType type;
+	int stock;
+
+public:
+	Publication();
+	void storePublication(string itemTitle, string itemPublisher, double itemPrice, 
+		int itemYear, PublicationType itemType, int itemStock);
+	void displayInfo() const;
+	void checkOut();
+	void checkIn();
+	string getTitle() const;
+	int getStock() const;
+};
