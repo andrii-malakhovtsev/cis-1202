d462b67d10b719b7ac4fa0b3181a9124c0c0c9d5
Andrii <111363234+malandrii@users.noreply.github.com>
2024-11-26 23:10:36 -0500

    Vehicle parent class addition

---
+++ Week 14/Vehicle.h
@@ -0,0 +1,24 @@
+// Andrii Malakhovtsev
+// CIS 1202 501
+// November 26, 2024
+
+#include <string>
+
+using namespace std;
+
+class Vehicle {
+protected:
+    string manufacturer;
+    int yearBuilt;
+
+public:
+    Vehicle(const string& manufacturer = "", int yearBuilt = 0);
+
+    string getManufacturer() const;
+    int getYearBuilt() const;
+
+    void setManufacturer(const string& manufacturer);
+    void setYearBuilt(int yearBuilt);
+
+    virtual void displayInfo() const;
+};
