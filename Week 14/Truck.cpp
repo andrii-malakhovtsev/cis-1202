532e23561f090ac8bd554a8ebc67616245144df3
Andrii <111363234+malandrii@users.noreply.github.com>
2024-11-28 23:03:45 -0500

    Truck child class from Vehicle addition

---
+++ Week 14/Truck.cpp
@@ -0,0 +1,21 @@
+﻿// Andrii Malakhovtsev
+// CIS 1202 501
+// November 28, 2024
+
+#include "Truck.h"
+#include <iostream>
+
+Truck::Truck(const string &manufacturer, int yearBuilt, int towingCapacity) : Vehicle(manufacturer, yearBuilt), towingCapacity(towingCapacity) {}
+
+int Truck::getTowingCapacity() const {
+    return towingCapacity;
+}
+
+void Truck::setTowingCapacity(int towingCapacity) {
+    this->towingCapacity = towingCapacity;
+}
+
+void Truck::displayInfo() const {
+    Vehicle::displayInfo();
+    cout << "Towing Capacity: " << towingCapacity << endl;
+}
