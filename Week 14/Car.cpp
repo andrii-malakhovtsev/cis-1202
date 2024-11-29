c06e85c5b3007cf00ec88bfb7e0ebf9b82a32bd0
Andrii <111363234+malandrii@users.noreply.github.com>
2024-11-28 23:03:05 -0500

    Car child class from Vehicle addition

---
+++ Week 14/Car.cpp
@@ -0,0 +1,21 @@
+﻿// Andrii Malakhovtsev
+// CIS 1202 501
+// November 28, 2024
+
+#include "Car.h"
+#include <iostream>
+
+Car::Car(const string &manufacturer, int yearBuilt, int numberOfDoors) : Vehicle(manufacturer, yearBuilt), numberOfDoors(numberOfDoors) {}
+
+int Car::getNumberOfDoors() const {
+    return numberOfDoors;
+}
+
+void Car::setNumberOfDoors(int numberOfDoors) {
+    this->numberOfDoors = numberOfDoors;
+}
+
+void Car::displayInfo() const {
+    Vehicle::displayInfo();
+    cout << "Number of Doors: " << numberOfDoors << endl;
+}
